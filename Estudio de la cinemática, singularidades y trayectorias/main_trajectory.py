3094cf0e9dc05e4045c35fa9263ed54c9f347aae
Mario Punter <167004883+ElPuma30@users.noreply.github.com>
2025-03-04 19:43:47 +0100

    Update main_trajectory.py

--- Estudio de la cinemática, singularidades y trayectorias/main_trajectory.py
+++ Estudio de la cinemática, singularidades y trayectorias/main_trajectory.py
@@ -7,23 +7,23 @@ from trajectory_singularities import *
 import sys 
 
 with open("salida.txt", "w") as archivo:
-    # Redirige la salida estándar al archivo
+    # Redirige la salida estandar al archivo
     sys.stdout = archivo  
     # Fijamos la trayectoria a analizar
     trajectory = trajectory_TEST
 
-    # Obtenemos las soluciones analíticas para cada waypoint
+    # Obtenemos las soluciones analiticas para cada waypoint
     waypoint_solutions = get_analitic_solutions_for_trajectory(trajectory, dh_params)
     write_waypoint_information(trajectory, waypoint_solutions)
 
-    # Refinamos las soluciones numéricamente (True/False)
-    refine_numerically = True # 
+    # Refinamos las soluciones numericamente (True/False)
+    refine_numerically = True 
     if refine_numerically:
         solutions = refine_solutions_numerically(trajectory, waypoint_solutions, dh_params)
     else:
         solutions = waypoint_solutions
 
-    # Determinamos la mejor cadena para cada solución inicial
+    # Determinamos la mejor cadena para cada solucion inicial
     best_by_start, global_best_trajectory, global_best_cost = find_best_chains_by_start(solutions)
     write_best_trajectory(best_by_start, global_best_trajectory, global_best_cost)
 
